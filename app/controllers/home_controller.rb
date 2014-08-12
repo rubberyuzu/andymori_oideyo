f484afc7c80fadb6c9b3358511d9a75627d6fef5
rubberyuzu <yuzus@google.com>
2014-08-12 16:18:21 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 	def index
 		@lines = Line.all	
 		@comment = Comment.new
-		@comments = Comment.all.sort_by &:created_at
+		@comments = Comment.all
+		@comments = @comments.sort_by &:created_at
 		@general_comment = GeneralComment.new
 		@general_comments = GeneralComment.all
 	end
