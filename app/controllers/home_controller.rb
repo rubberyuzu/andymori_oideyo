ca79798955925444952a628885406b7563762edb
rubberyuzu <yuzus@google.com>
2014-08-12 16:21:49 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 		@lines = Line.all	
 		@comment = Comment.new
 		@comments = Comment.all
-		@comments = @comments.sort_by &:created_at
+		@comments = @comments.sort { |a,b| a.created_at <=> b.created_at }
 		@general_comment = GeneralComment.new
 		@general_comments = GeneralComment.all
 	end
